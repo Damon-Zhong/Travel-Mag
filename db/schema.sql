b6b3e56fa63d5e5d84d5ac919ba6ca91c8d81fa5
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-07-02 14:44:28 -0400

    Added schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS destinations;
+CREATE DATABASE destinations;
+USE destinations;
+
+CREATE TABLE cities (
+    id INT AUTO_INCREMENT NOT NULL,
+    city_name varchar(255),
+    city_country varchar(255),
+    sights_link varchar(255),
+    sights_headline varchar(255),
+    eats_link varchar(255),
+    eats_headline varchar(255),
+    bars_link varchar(255),
+    bars_headline varchar(255),
+    hotels_link varchar(255),
+    hotels_headline varchar(255),
+    free_link varchar(255),
+    free_headline varchar(255),
+    nightlife_link varchar(255),
+    nightlife_headline varchar(255),
+    family_link varchar(255),
+    family_headline varchar(255),
+    PRIMARY KEY (id)
+);
