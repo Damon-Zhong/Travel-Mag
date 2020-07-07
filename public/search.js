6868f8ffb346976c3a7207ff159624b93d00ab6f
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-07-07 10:23:08 -0400

    Fixed merge conflicts

--- public/search.js
+++ public/search.js
@@ -39,7 +39,6 @@ async function searchCities(event) {
     var userInput = document.getElementById("search-box").value;
     let response = await fetch(`/api/search/?q=${userInput}`)
         .then(
-<<<<<<< HEAD
             function(response) {
             if (response.status !== 200) {
                 console.log('Looks like there was a problem. Status Code: ' +
@@ -62,30 +61,6 @@ async function searchCities(event) {
                     $('#cityChoiceModal').modal();
                     document.getElementById("modal-body").innerHTML =
                     `<p>We couldn't find a city that matches your query in our database.
-=======
-            function (response) {
-                if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' +
-                        response.status);
-                    return;
-                }
-                response.json().then(function (data) {
-                    if (data.length == 1) {
-                        var city = data[0];
-                        window.location.replace(`/cities.html#${city.id}`);
-                    } else if (data.length > 1) {
-                        $('#cityChoiceModal').modal();
-                        document.getElementById("modal-body").innerHTML =
-                            `<p>We've found multiple cities that match your query, please choose:</p>`
-                        data.forEach(city => {
-                            document.getElementById("modal-body").innerHTML +=
-                                `<p><a href="/cities.html#${city.id}" alt="Details on ${city.city_name}">${city.city_name}</a></p>`
-                        });
-                    } else {
-                        $('#cityChoiceModal').modal();
-                        document.getElementById("modal-body").innerHTML =
-                            `<p>We couldn't find a city that matches your query in our database.
->>>>>>> develop
                     <br><br>
                     Do you or someone you know want to add the city? Go <a href="/submitCity.html" 
                     alt="Add city">here</a> to enter the city's details.<p>`
